30198c0456b91af55f57406085d9fc08061219db
otienonick <nick.odongi@student.moringaschool.com>
2022-05-06 12:03:38 +0300

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,14 @@
+from pickle import TRUE
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="todolist",null=TRUE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
